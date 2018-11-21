7f8413f7abc9c6ef133204feed5f6df230ee99e6
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2018-11-21 03:20:50 +0300

    Added some unit tests for LogWatch and Node classes

--- watcher.py
+++ watcher.py
@@ -9,10 +9,63 @@ def main():
 
     log_file = "/var/log/syslog"
     instance = LogWatch(log_file)
-    instance.setMatch(8)
-    instance.combineMatch(6, "AND", ())
+    tester(instance, log_file)
+
     print("Log source path of the object:", instance.log_source_path)
     print("Rule tree of the object:\n", json.dumps(instance.rule_tree, indent = 2))
+
+
+def tester(instance, log_file):
+    print("Initialized a LogWatch object with " + log_file + " file")
+    assert instance.log_source_path == "/var/log/syslog"
+
+    print("Constructing rule tree using setMatch and combineMatch")
+    instance.setMatch(8)
+    instance.combineMatch(6, "AND", ())
+    assert instance.rule_tree.value == "AND"
+    assert instance.rule_tree.left.value == 8 and instance.rule_tree.right.value == 6
+
+    instance.combineMatch(5, "OR", (0,))
+    instance.combineMatch(9, "OR", (0, 0))
+    assert instance.rule_tree.value == "AND"
+    assert instance.rule_tree.left.value == "OR" and instance.rule_tree.right.value == 6
+    assert instance.rule_tree.left.left.value == "OR" and instance.rule_tree.left.right.value == 5
+    assert instance.rule_tree.left.left.left.value == 8 and instance.rule_tree.left.left.right.value == 9
+
+    tree_conf1 = "storage"
+    print("Saving current configuration as " + tree_conf1 + ".json")
+    instance.save(tree_conf1)
+    assert os.path.exists(tree_conf1 + ".json") == 1
+
+    print("Loading current configuration from " + tree_conf1 + ".json")
+    instance.load(tree_conf1)
+    # [AND,[OR,[OR,[8,[],[]],[9,[],[]]],[5,[],[]]],[6,[],[]]]
+    assert instance.rule_tree.value == "AND"
+    assert instance.rule_tree.left.value == "OR" and instance.rule_tree.right.value == 6
+    assert instance.rule_tree.left.left.value == "OR" and instance.rule_tree.left.right.value == 5
+    assert instance.rule_tree.left.left.left.value == 8 and instance.rule_tree.left.left.right.value == 9
+
+    del_address = (0, 0, 1)
+    print("Deleting node at " + str(del_address) + " address")
+    instance.delMatch(del_address)
+    # [AND,[OR,[8,[],[]],[5,[],[]],[6,[],[]]]
+    assert instance.rule_tree.value == "AND"
+    assert instance.rule_tree.left.value == "OR" and instance.rule_tree.right.value == 6
+    assert instance.rule_tree.left.left.value == 8 and instance.rule_tree.left.right.value == 5
+
+    tree_conf2 = "storage2"
+    print("Saving current configuration as " + tree_conf2 + ".json")
+    instance.save(tree_conf2)
+    assert os.path.exists(tree_conf2 + ".json") == 1
+
+    print("Loading current configuration from " + tree_conf2 + ".json")
+    instance.load(tree_conf2)
+    # [AND,[OR,[8,[],[]],[5,[],[]],[6,[],[]]]
+    assert instance.rule_tree.value == "AND"
+    assert instance.rule_tree.left.value == "OR" and instance.rule_tree.right.value == 6
+    assert instance.rule_tree.left.left.value == 8 and instance.rule_tree.left.right.value == 5
+
+    print("Test finished successfully...\n")
 
 
 class LogWatch:
