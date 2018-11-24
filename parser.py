c5e228cf0cfe86d10b7e1851061a4bf79d862595
Halit Simsek <mail.simsekhalit@gmail.com>
2018-11-24 17:36:33 +0300

    Parser class is implemented.

---
+++ parser.py
@@ -0,0 +1,39 @@
+from pyparsing import Word, alphas, Suppress, Combine, nums, string, Optional, Regex
+import time
+
+
+class Parser(object):
+    def __init__(self):
+        ints = Word(nums)
+
+        # timestamp
+        month = Word(string.ascii_uppercase, string.ascii_lowercase, exact=3)
+        day   = ints
+        hour  = Combine(ints + ":" + ints + ":" + ints)
+
+        timestamp = month + day + hour
+
+        # hostname
+        hostname = Word(alphas + nums + "_" + "-" + ".")
+
+        # appname
+        appname = Word(alphas + "/" + "-" + "_" + ".") + Optional(Suppress("[") + ints + Suppress("]")) + Suppress(":")
+
+        # message
+        message = Regex(".*")
+
+        # pattern build
+        self.__pattern = timestamp + hostname + appname + message
+
+    def parse(self, line):
+        parsed = self.__pattern.parseString(line)
+
+        payload              = {}
+        payload["timestamp"] = int(time.mktime(time.strptime(str(time.localtime().tm_year) + " " + ' '.join(parsed[:3]),
+                                                             "%Y %b %d %H:%M:%S")))
+        payload["hostname"]  = parsed[3]
+        payload["appname"]   = parsed[4]
+        payload["pid"]       = parsed[5]
+        payload["message"]   = parsed[6]
+
+        return payload
