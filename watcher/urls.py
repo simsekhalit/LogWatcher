c89fbc995a920d6b80a6f42c5d3b4173c353e154
ysyesilyurt <yesilyurt.selim@metu.edu.tr>
2018-12-31 01:21:05 +0300

    Fixed bugs.

--- watcher/urls.py
+++ watcher/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
-    path("create/", views.create, name='create'),
+    path("create/", views.index, {"create": True}, name="create"),
     re_path(r"^logs/(?P<wid>[0-9]+)$", views.logs, name='logs'),
     re_path(r"^rules/(?P<wid>[0-9]+)$", views.rules, name='rules')
 ]
