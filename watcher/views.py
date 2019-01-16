0a5377914226f491006f3e9662d080760e417ec0
Halit Simsek <mail.simsekhalit@gmail.com>
2019-01-16 17:15:00 +0300

    LogWatch Manager core refactor.

--- watcher/views.py
+++ watcher/views.py
@@ -1,123 +1,132 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import os
+import socket
+import sqlite3
+import sys
 
-from watcher.models import Watchers, WatcherLogs, WatcherRules
-from util import UDSBuffer, reverseAddress
-
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../util.py")))
-
-
-# Create your views here.
-
-
-@login_required
-def logs(request, wid=None):
-	"""Shows filtered logs of corresponding Watcher instance."""
-	filteredLogs = []
-	queryRes = WatcherLogs.objects.filter(wid=wid)
-	for queryLog in queryRes:
-		filteredLogs.append({"log": queryLog.log})
-	return render(request, "logs.html", {"filteredLogs": filteredLogs, "lw": "Logwatch " + str(wid)})
-
-
-@login_required
-def rules(request, wid=None):
-	"""Shows and updates rules of a Watcher instance."""
-	try:
-		if request.POST['submit'] == 'SetMatch':
-			path = request.POST['path']
-			if path == 'NONE':
-				path = '()'
-			rule = request.POST['rule']
-			buffer = UDSBuffer()
-			buffer.send("setMatch\n" + wid + "\n" + rule + "\n" + path)
-			response = buffer.recv()
-			_rules = getRules(wid)
-			if response == '0':
-				return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid),
-													  "op": "setMatch Operation was successful."})
-			elif response == '2':
-				return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid),
-													  "op": "Please fill fields with correct types"})
-			else:
-				return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid),
-													  "op": "setMatch Operation was not successful."})
-		elif request.POST['submit'] == 'CombineMatch':
-			path = request.POST['path']
-			connector = request.POST['connector']
-			if path == 'NONE':
-				path = '()'
-			rule = request.POST['rule']
-			buffer = UDSBuffer()
-			buffer.send("combineMatch\n" + wid + "\n" + rule + "\n" + connector + "\n" + path)
-			response = buffer.recv()
-			_rules = getRules(wid)
-			if response == '0':
-				return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid),
-													  "op": "CombineMatch Operation was successful."})
-			elif response == '2':
-				return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid),
-													  "op": "Please fill fields with correct types"})
-			else:
-				return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid),
-													  "op": "CombineMatch Operation was not successful."})
-		elif request.POST['submit'] == 'DelMatch':
-			path = request.POST['path']
-			if path == 'NONE':
-				path = '()'
-			buffer = UDSBuffer()
-			buffer.send("delMatch\n" + wid + "\n" + path)
-			response = buffer.recv()
-			_rules = getRules(wid)
-			if response == '0':
-				return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid),
-													  "op": "DelMatch Operation was successful."})
-			elif response == '2':
-				return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid),
-													  "op": "Please fill fields with correct types"})
-			else:
-				return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid),
-													  "op": "DelMatch Operation was not successful."})
-		else:
-			return render(request, 'error.html', {'message': 'Invalid request'})
-	except KeyError:
-		_rules = getRules(wid)
-		return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(wid)})
+sys.path.insert(1, os.path.dirname(__file__,))
+from watcher.util import databasePath, UDSAddr, SocketBuffer
 
 
 @login_required
 def index(request, create=None):
-	"""Home page which shows active Watcher instances."""
-	if create:
-		buffer = UDSBuffer()
-		buffer.send("create")
-		response = buffer.recv()
-		watchers = getWatchers()
-		if response == '0':
+    """Home page which shows active Watcher instances."""
+    if create:
+        buffer = UDSBuffer()
+        buffer.send("create")
+        response = buffer.recv()
+        if response == '0':
+            return render(request, "index.html", {"watchers": getWatchers(), "create": "A new Logwatch instance successfully created."})
+        else:
+            return render(request, "index.html", {"watchers": getWatchers(), "create": "Failed to create a new Logwatch instance."})
+    else:
+        return render(request, "index.html", {"watchers": getWatchers()})
 
-			return render(request, "index.html", {"watchers": watchers, "create": "A new Logwatch instance successfully created."})
-		else:
-			return render(request, "index.html", {"watchers": watchers, "create": "Failed to create a new Logwatch instance."})
-	else:
-		watchers = getWatchers()
-		return render(request, "index.html", {"watchers": watchers})
+
+@login_required
+def logs(request, lwID=None):
+    """Shows filtered logs of corresponding Watcher instance."""
+    # TODO: Check if lwID exists
+    return render(request, "logs.html", {"logs": getLogs(lwID), "name": getWatcher(lwID)[0]})
+
+
+@login_required
+def rules(request, lwID=None):
+    """Shows and updates rules of a Watcher instance."""
+    if request.method == "GET":
+        _rules = getRules(lwID)
+        return render(request, "rules.html", {"rules": _rules, "lw": "LogWatch " + str(lwID)})
+    elif request.POST['submit'] == 'SetMatch':
+        path = request.POST['path']
+        if path == 'NONE':
+            path = '()'
+        rule = request.POST['rule']
+        buffer = UDSBuffer()
+        buffer.send("setMatch\n" + lwID + "\n" + rule + "\n" + path)
+        response = buffer.recv()
+        _rules = getRules(lwID)
+        if response == '0':
+            return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(lwID),
+                                                  "op": "setMatch Operation was successful."})
+        elif response == '2':
+            return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(lwID),
+                                                  "op": "Please fill fields with correct types"})
+        else:
+            return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(lwID),
+                                                  "op": "setMatch Operation was not successful."})
+    elif request.POST['submit'] == 'CombineMatch':
+        path = request.POST['path']
+        connector = request.POST['connector']
+        if path == 'NONE':
+            path = '()'
+        rule = request.POST['rule']
+        buffer = UDSBuffer()
+        buffer.send("combineMatch\n" + lwID + "\n" + rule + "\n" + connector + "\n" + path)
+        response = buffer.recv()
+        _rules = getRules(lwID)
+        if response == '0':
+            return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(lwID),
+                                                  "op": "CombineMatch Operation was successful."})
+        elif response == '2':
+            return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(lwID),
+                                                  "op": "Please fill fields with correct types"})
+        else:
+            return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(lwID),
+                                                  "op": "CombineMatch Operation was not successful."})
+    elif request.POST['submit'] == 'DelMatch':
+        path = request.POST['path']
+        if path == 'NONE':
+            path = '()'
+        buffer = UDSBuffer()
+        buffer.send("delMatch\n" + lwID + "\n" + path)
+        response = buffer.recv()
+        _rules = getRules(lwID)
+        if response == '0':
+            return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(lwID),
+                                                  "op": "DelMatch Operation was successful."})
+        elif response == '2':
+            return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(lwID),
+                                                  "op": "Please fill fields with correct types"})
+        else:
+            return render(request, "rules.html", {"rules": _rules, "lw": "Logwatch " + str(lwID),
+                                                  "op": "DelMatch Operation was not successful."})
+    else:
+        return render(request, 'error.html', {'message': 'Invalid request'})
+
+
+def getLogs(lwID):
+    with sqlite3.connect(os.path.join(os.path.dirname(__file__), databasePath)) as conn:
+        c = conn.cursor()
+        data = c.execute("""select log from logs where wid == ?""", (lwID,)).fetchall()
+    return [log[0] for log in data]
+
+
+def getRules(lwID):
+    with sqlite3.connect(os.path.join(os.path.dirname(__file__), databasePath)) as conn:
+        c = conn.cursor()
+        return c.execute("""select rules from rules where wid == ?""", (lwID,)).fetchone()[0]
+
+
+def getWatcher(lwID):
+    with sqlite3.connect(os.path.join(os.path.dirname(__file__), databasePath)) as conn:
+        c = conn.cursor()
+        return c.execute("""select name from watchers where wid == ?""", (lwID,)).fetchone()
 
 
 def getWatchers():
-	watchers = []
-	queryWatcher = Watchers.objects.all()
-	for watcher in queryWatcher:
-		_rules = len(WatcherRules.objects.filter(wid=watcher.wid))
-		_logs = len(WatcherLogs.objects.filter(wid=watcher.wid))
-		watchers.append({"wid": watcher.wid, "name": watcher.name, "rules": _rules, "logs": _logs})
-	return watchers
+    ret = []
+    with sqlite3.connect(os.path.join(os.path.dirname(__file__), databasePath)) as conn:
+        c = conn.cursor()
+        watchers = c.execute("""select wid, name from watchers""").fetchall()
+        for watcher in watchers:
+            logc = c.execute("""select count(log) from logs where wid == ?""", (watcher[0],)).fetchone()[0]
+            rulec = getRules(watcher[0]).count("{'value': ")
+            ret.append({"wid": watcher[0], "name": watcher[1], "rules": rulec, "logs": logc})
+    return ret
 
 
-def getRules(wid):
-	_rules = []
-	queryRes = WatcherRules.objects.filter(wid=wid).order_by('rule_id')
-	for queryRule in queryRes:
-		path = tuple(reverseAddress(queryRule.rule_id))
-		_rules.append({"rule": queryRule.rule, "path": path})
-	return _rules
+def UDSBuffer():
+    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock.connect(os.path.join(os.path.dirname(__file__), UDSAddr))
+    return SocketBuffer(sock, UDSAddr)
